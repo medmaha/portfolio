3651d28ad0c4f7c8976d902f2006001069f4deb8
= <=>
2023-04-02 16:15:06 +0000

    update social media links ..

--- src/component/utils/SocialMedia.jsx
+++ src/component/utils/SocialMedia.jsx
@@ -11,7 +11,7 @@ export default function SocialMedia() {
     return (
         <>
             <div className="fixed z-10 primary-bg top-[65px] left-[0] w-full max-w-[35px] md:max-w-[50px] h-[calc(100vh-80px)]">
-                <ul className="h-full flex justify-between gap-10 flex-col ">
+                <ul className="h-full flex justify-around gap-10 flex-col ">
                     <li className="flex justify-center">
                         <a
                             className=""
