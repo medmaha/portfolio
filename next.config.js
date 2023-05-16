c4c24c15cb7d7b7bae6411c61d3e0e65e2dcb151
= <=>
2023-05-16 02:10:38 +0000

    switched from plain react to nextjs

--- next.config.js
+++ next.config.js
@@ -7,12 +7,6 @@ const nextConfig = {
         OPEN_AI_KEY: process.env.OPEN_AI_KEY,
         WEBSOCKET_URL: process.env.WEBSOCKET_URL,
     },
-    images: {
-        domains: (() => {
-            const domain = process.env.IMAGE_DOMAINS
-            return domain.split(",")
-        })(),
-    },
 }
 
 module.exports = nextConfig
