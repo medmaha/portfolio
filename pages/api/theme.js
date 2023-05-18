8a78becbc6b153f54cfe3357cfa738c634035db8
Mahamed Toure <104234458+medmaha@users.noreply.github.com>
2023-05-18 12:06:59 +0000

    Updated theme cookies storage

--- pages/api/theme.js
+++ pages/api/theme.js
@@ -10,9 +10,16 @@ export default async function handle(req, res) {
 
     const prodENV = process.env.NODE_ENV === "development"
 
+
+const currentDate = new Date();
+
+const expirationDate = new Date(currentDate.getTime() + (30 * 24 * 60 * 60 * 1000));
+
+const expires = "expires="expirationDate.toUTCString();
+
     res.setHeader(
         "set-cookie",
-        `theme=${themeClass};httpOnly=true;sameSite=strick;path=/;secure=${prodENV}`,
+        `theme=${themeClass};httpOnly=true;sameSite=strick;secure=${prodENV};${expires}`,
     )
     res.setHeader("content-type", "application/json")
     res.status(200).json({ "current-theme": themeClass })
